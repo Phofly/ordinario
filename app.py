988e4d7fc6989b99c872f95099bf2ede558c48f1
Angel <phorflace@gmail.com>
2025-07-03 08:18:12 -0600

    app

--- app.py
+++ app.py
@@ -1,25 +1,7 @@
-from flask import Flask, render_template, request, redirect
-import csv
-from datetime import datetime
+@app.route('/')
+def index():
+    return 'Index Page'
 
-app = Flask(_name_)
-
-@app.route("/ordinario", methods=["GET", "POST"])
-def ordinario():
-    if request.method == "POST":
-        fecha = datetime.today().strftime('%Y-%m-%d')
-        nombre = request.form["nombre"]
-        correo = request.form["correo"]
-        telefono = request.form["telefono"]
-        pin = request.form["pin"]
-        medio = request.form["medio"]
-        equipo = request.form["equipo"]
-        reparacion = request.form["reparacion"]
-        estatus = request.form["estatus"]
-
-        with open("prospectos.csv", mode="a", newline="", encoding="utf-8") as archivo:
-            escritor = csv.writer(archivo)
-            escritor.writerow([fecha, nombre, correo, telefono, pin, medio, equipo, reparacion, estatus])
-
-        return redirect("/ordinario")
-return render_template("formulario.html")
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
